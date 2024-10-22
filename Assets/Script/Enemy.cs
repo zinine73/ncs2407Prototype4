8135ab6be28c0b63d3ed3a460a780752a46269e7
zinine73 <zinine@naver.com>
2024-10-22 15:42:51 +0900

    prototype 4

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -19,5 +19,10 @@ public class Enemy : MonoBehaviour
         Vector3 lookDirection = (player.transform.position 
             - transform.position).normalized;
         rb.AddForce(lookDirection * speed);
+
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject enemyPrefab;
     private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
 
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), 
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(),
+            powerupPrefab.transform.rotation);
+    }
+
+    private void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(),
+            powerupPrefab.transform.rotation);
+        }
+    }
+
+    private void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), 
             enemyPrefab.transform.rotation);
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
